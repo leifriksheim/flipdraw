5b67305680cdcbcf0488d129ec554d8031598251
leifriksheim <riksheim.leif@gmail.com>
2018-02-10 18:57:31 +0100

    Updated firebase configs

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,10 @@
 const config = {
-  apiKey: "<YOUR-API-KEY>",
-  databaseURL: "https://<YOUR-DATABASE-NAME>.firebaseio.com",
-  authDomain: "<YOUR-AUTH-DOMAIN>",
-  storageBucket: "<YOUR-STORAGE-BUCKET>.appspot.com"
+  apiKey: "AIzaSyBZ9y6o6UZK90deZlMbuLIbj7bdghQ7fAE",
+  authDomain: "flipdraw-js.firebaseapp.com",
+  databaseURL: "https://flipdraw-js.firebaseio.com",
+  projectId: "flipdraw-js",
+  storageBucket: "flipdraw-js.appspot.com",
+  messagingSenderId: "1044760879147"
 };
 
 firebase.initializeApp(config);
