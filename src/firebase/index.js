c4d9552d7b53cdb820ec191a27cd121cab2d54ab
leifriksheim <riksheim.leif@gmail.com>
2018-02-10 23:31:27 +0100

    Added findDrawing function

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,10 +4,14 @@ import config from './config.js';
 firebase.initializeApp(config);
 
 export function submitDrawing({ drawingId, bodyPart, drawingData}) {
+
+  const drawing = {isActive: false};
+  drawing[bodyPart] = drawingData;
+
   return firebase
     .database()
-    .ref(`drawings/${drawingId}/${bodyPart}`)
-    .set(drawingData);
+    .ref(`drawings/${drawingId}`)
+    .set(drawing);
 }
 
 export function createUser(userName) {
@@ -16,5 +20,20 @@ export function createUser(userName) {
 }
 
 
-export function createNewDrawing() {
+export async function findDrawing() {
+  // Only get the drawings that are not active (currently being drawn by someone else)
+  const drawingsFromFirebase = await firebase.database()
+                                 .ref(`drawings`)
+                                 .orderByChild('isActive')
+                                 .equalTo(false)
+                                 .once('value');
+
+  const drawings = drawingsFromFirebase.val();
+  if (drawings) {
+    // Return the first drawing of existing drawings
+    return drawings[Object.keys(drawings)[0]];
+  } else {
+    // Return false if no drawings exist
+    return false
+  }
 }
