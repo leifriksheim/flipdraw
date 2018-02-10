42abe54873637c83c1ecea24450937eef6eea295
leifriksheim <riksheim.leif@gmail.com>
2018-02-10 22:52:29 +0100

    Added StartScreen with input for user and startGame button

--- src/routes/StartScreen.js
+++ src/routes/StartScreen.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import autoBind from 'react-autobind';
+import { createUser } from '../firebase';
 
-function StartScreen() {
-  return (
-    <div>Halla</div>
-  );
+class StartScreen extends React.Component {
+  constructor() {
+    super();
+    autoBind(this);
+    this.state = {
+      userName: '',
+    };
+  }
+
+  updateUsername(e) {
+    const userName = e.target.value;
+    this.setState({
+      userName: userName
+    });
+  }
+
+  startGame() {
+    const userKey = createUser(this.state.userName);
+    console.log(userKey);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.userName} onChange={this.updateUsername} />
+        <button onClick={this.startGame}>Start Drawing!</button>
+      </div>
+    )
+  }
 }
 
 export default StartScreen;
