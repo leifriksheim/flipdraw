f26d4e7de6e72223a04da8ccdc6995f0de63e739
leifriksheim <riksheim.leif@gmail.com>
2018-02-11 21:26:27 +0100

    Added thank-you screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import StartScreen from "./routes/StartScreen.js";
 import DrawingScreen from "./routes/DrawingScreen.js";
+import ThankYouScreen from "./routes/ThankYouScreen.js";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={StartScreen} />
           <Route path="/draw/:id" component={DrawingScreen} />
+          <Route path="/thank-you" component={ThankYouScreen} />
         </Switch>
       </main>
     );
---
+++ src/routes/ThankYouScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import autoBind from "react-autobind";
+import { withRouter } from "react-router-dom";
+import { createUser, findDrawing, createNewDrawing } from "../firebase";
+import * as firebase from "firebase";
+import Button from "../components/Button";
+
+class ThankYouScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+    this.state = {
+      userName: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="section --full-height --center-v --center-h">
+        <h1 class="logo">
+          <span class="logo__flipped">Thank</span>You!
+        </h1>
+        <Button>Draw a new one</Button>
+        <Button>See all your drawings</Button>
+      </div>
+    );
+  }
+}
+
+export default ThankYouScreen;
