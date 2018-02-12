95417e3881055d406d9d8a590d70e9ff5fac5103
leifriksheim <riksheim.leif@gmail.com>
2018-02-12 23:25:08 +0100

    Moved some logic to DrawingScreen instead of DrawArea

--- src/routes/DrawingScreen.js
+++ src/routes/DrawingScreen.js
@@ -1,25 +1,40 @@
 import React from "react";
 import DrawArea from "../components/DrawArea";
 import autoBind from "react-autobind";
-import {
-  submitDrawing,
-  getRandomBodypart
-} from "../firebase/";
+import { firebase, db, submitDrawing, getRandomBodypart } from "../firebase/";
 
 class DrawingScreen extends React.Component {
   constructor(props) {
     super(props);
     autoBind(this);
     this.state = {
-      bodyPart: "",
+      bodyPart: ""
     };
   }
 
   async componentDidMount() {
-    const bodyPart = await getRandomBodypart(this.props.match.params.id);
+    // Initial data
+    const drawingId = this.props.match.params.id;
+    const bodyPart = await getRandomBodypart(drawingId);
+    if (!bodyPart) {
+      this.props.history.push("/");
+    }
+    // Set the random body part to state
     this.setState({
       bodyPart: bodyPart
     });
+    // Set the given bodyPart to inProgress so other people can't draw on the same part
+    this.inProgress = db.ref(
+      `drawings/${drawingId}/parts/${bodyPart}/inProgress`
+    );
+    this.inProgress.set(true);
+    // Remove inProgress status if user logs off without finishing
+    this.inProgress.onDisconnect().set(false);
+  }
+
+  componentDidUnmount() {
+    // Remove inProgress status if user submits drawing or leaves the route
+    this.inProgress.set(false);
   }
 
   submit(drawingData) {
