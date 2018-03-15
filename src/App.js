76596894696c1b2994869c1f71528ca4aedd4bf7
bard <bnhovde@gmail.com>
2018-03-15 22:44:24 +0100

    FIX - Auth on reload now works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Switch, Route, Redirect } from "react-router-dom";
 import autoBind from "react-autobind";
 import { isAuthenticated } from "./firebase";
 
+// Components
+import View from "./components/View";
+import Loader from "./components/Loader";
 import AuthHOC from "./components/AuthHOC";
+
+// Routes
 import StartScreen from "./routes/StartScreen";
 import DrawingScreen from "./routes/DrawingScreen";
 import ThankYouScreen from "./routes/ThankYouScreen";
@@ -19,36 +25,66 @@ class App extends Component {
     this.state = {};
   }
 
+  static contextTypes = {
+    authLoading: PropTypes.bool.isRequired
+  };
+
   render() {
+    const { authLoading } = this.context;
     return (
       <main className="main">
         <Switch>
           <Route exact path="/" component={StartScreen} />
-          <ProtectedRoute path="/draw/:id" component={DrawingScreen} />
-          <ProtectedRoute path="/drawing/:id" component={SingleDrawing} />
-          <ProtectedRoute path="/thank-you" component={ThankYouScreen} />
-          <ProtectedRoute path="/all-drawings" component={AllDrawings} />
+          <Route path="/drawing/:id" component={SingleDrawing} />
+          <ProtectedRoute
+            authLoading={authLoading}
+            path="/draw/:id"
+            component={DrawingScreen}
+          />
+          <ProtectedRoute
+            authLoading={authLoading}
+            path="/thank-you"
+            component={ThankYouScreen}
+          />
+          <ProtectedRoute
+            authLoading={authLoading}
+            path="/all-drawings"
+            component={AllDrawings}
+          />
         </Switch>
       </main>
     );
   }
 }
 
-const ProtectedRoute = ({ component: Component, ...rest }) => (
+const ProtectedRoute = ({ component: Component, authLoading, ...rest }) => (
   <Route
     {...rest}
-    render={renderProps =>
-      isAuthenticated() ? (
-        <Component {...renderProps} />
-      ) : (
+    render={renderProps => {
+      // If loading, show loader
+      if (authLoading) {
+        return (
+          <View isVisible isVcentered>
+            <Loader />
+          </View>
+        );
+      }
+
+      // If user is authenticated, go to route
+      if (isAuthenticated()) {
+        return <Component {...renderProps} />;
+      }
+
+      // Otherwise, redirect to home
+      return (
         <Redirect
           to={{
             pathname: "/",
             state: { from: renderProps.location }
           }}
         />
-      )
-    }
+      );
+    }}
   />
 );
 
