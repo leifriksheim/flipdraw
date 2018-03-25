620146a27f927bf7e32b9270ce30a6e4b05a68ee
leifriksheim <riksheim.leif@gmail.com>
2018-03-25 23:20:47 +0200

    Added notification count for not yet viewed drawings

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,22 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import autoBind from "react-autobind";
+import { db, auth } from "../../firebase";
 import "./index.css";
 
 import cx from "classnames";
 
-function Avatar({ isVisible, user }) {
-  const avatarClass = cx({
-    avatar: true,
-    "--visible": isVisible
-  });
+class Avatar extends React.Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+    this.state = {
+      newDrawings: 0
+    };
+  }
 
-  const name = (user && user.displayName) || "?";
+  componentWillReceiveProps(nextProps) {
+    const uid = nextProps.user ? nextProps.user.uid : false;
+    if(uid) {
+      db.ref(`users/${uid}/drawings`).orderByChild('viewed').equalTo(false).on('value', snapshot => {
+        const newDrawings = snapshot.numChildren();
+        this.setState({newDrawings})
+      })
+    }
+  }
 
-  return (
-    <Link to="/all-drawings" className={avatarClass}>
-      {name.charAt(0)}
-    </Link>
-  );
+  render() {
+    const avatarClass = cx({
+      avatar: true,
+      "--visible": this.props.isVisible
+    });
+
+    const notificationCount = this.state.newDrawings;
+
+    const notificationClass = cx({
+      "avatar__notification": true,
+      "--visible": (notificationCount > 0)
+    });
+
+    const name = (this.props.user && this.props.user.displayName) || "?";
+
+    return (
+      <Link to="/all-drawings" className={avatarClass}>
+        <span className={notificationClass}>{notificationCount}</span>
+        {name.charAt(0)}
+      </Link>
+    )
+  };
 }
 
 export default Avatar;
